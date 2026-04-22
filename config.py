af05a875591a0bbd62f09aab69d26d7e1346bb3c
jasmineyt682-source <jasmineyt682@gmail.com>
2026-04-22 22:49:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,18 @@ import os
 
 TOKEN = os.getenv("TOKEN")
 ADMIN_ID = os.getenv("ADMIN_ID")
+MONGO_URL = os.getenv("MONGO_URL")
 
-if TOKEN is None:
-    raise ValueError("TOKEN environment variable missing!")
+# clean TOKEN
+if TOKEN:
+    TOKEN = TOKEN.strip()
+else:
+    TOKEN = None
 
-if ADMIN_ID is None:
-    raise ValueError("ADMIN_ID environment variable missing!")
+# admin convert
+ADMIN_ID = int(ADMIN_ID) if ADMIN_ID and ADMIN_ID.isdigit() else 0
 
-ADMIN_ID = int(ADMIN_ID)
+if not MONGO_URL:
+    raise Exception("❌ MONGO_URL missing in Railway ENV")
+DEFAULT_START = "👋 Welcome to Premium Bot"
+DEFAULT_PRICE = "29"
